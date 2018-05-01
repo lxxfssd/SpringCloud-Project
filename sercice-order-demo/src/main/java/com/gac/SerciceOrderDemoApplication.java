5d351f2ef58305a82a012426cab5b5abf66bef6a
lxxfssd <lxxfssd@163.com>
2018-05-01 23:26:28 +0800

    ribbon集成hystrix

--- sercice-order-demo/src/main/java/com/gac/SerciceOrderDemoApplication.java
+++ sercice-order-demo/src/main/java/com/gac/SerciceOrderDemoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient  //服务采用其他注册中心。
+@EnableHystrix //启动断路器
 @SpringBootApplication
 //@EnableEurekaClient基于spring-cloud-netflix。服务采用eureka作为注册中心，使用场景较为单一。
 public class SerciceOrderDemoApplication {
--- sercice-order-demo/src/main/java/com/gac/service/MemberService.java
+++ sercice-order-demo/src/main/java/com/gac/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.gac.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,7 +14,14 @@ public class MemberService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "orderError") //fallbackMethod作为服务降级指点返回内容, @HystrixCommand服务发生错误，回调方法
     public List<String> getOrderByUserList() {
         return restTemplate.getForObject("http://service-member/getUserList", List.class);
+    }
+
+    private List<String> orderError() {
+        List<String> listUser = new ArrayList<String>();
+        listUser.add("not orderUser list");
+        return listUser;
     }
 }
